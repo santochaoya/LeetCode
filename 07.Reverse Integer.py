303b719151b03d37195420105831ef5132733301
Xiao Meng <santochaoya@yahoo.com.hk>
2021-03-30 22:55:43 +0800

    add signed number result

--- 07.Reverse Integer.py
+++ 07.Reverse Integer.py
@@ -1,25 +1,28 @@
 # ===============================================================================================
 # 
-# Given a signed 32-bit integer x, return x with its digits reversed. 
-# If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], 
+# Given a signed 32-bit integer abs_x, return abs_x with its digits reversed. 
+# If reversing abs_x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], 
 # then return 0.
 # 
 # ===============================================================================================
 
 x = input ('x = ')
 
-original_x = str(x)
-remain_len = len(original_x)
+abs_x = abs(x)
+original_abs_x = str(abs_x)
+remain_len = len(original_abs_x)
 result = 0
 
-if abs(x) > 2**31 - 1:
-    y = 0
-else:
-    while remain_len > 0:
-        digit_ten = len(original_x) - remain_len
-        quotient, x = divmod(x, 10 ** (remain_len - 1))
-        last_number = 10 ** digit_ten * quotient
-        result += last_number
-        remain_len -= 1
+while remain_len > 0:
+    digit_ten = len(original_abs_x) - remain_len
+    quotient, abs_x = divmod(abs_x, 10 ** (remain_len - 1))
+    last_number = 10 ** digit_ten * quotient
+    result += last_number
+    remain_len -= 1
 
+if abs(result) > 2*31-1:
+    result = 0
+elif x < 0:
+    result = -1 * result
+    
 print("Output: ", result)
